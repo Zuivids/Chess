40307628ddaf126db7a24ac31e9b684818f43526
Zuivids <112480628+Zuivids@users.noreply.github.com>
2022-09-07 21:46:55 +0300

    added new files

---
+++ src/main/Main.java
@@ -0,0 +1,24 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+      int x = 10;
+      x = x * x;
+      
+      if (x!=100) {
+    	  System.out.println("Ralfs tgd cringo");
+    	  
+      }
+      
+      else  { 
+    	  
+    	  System.out.println("Tgd laiks valo");
+    	  
+      }
+      
+		
+	}
+
+}
